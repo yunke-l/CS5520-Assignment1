4351a73a011247e3e62ed13be388de748549cfb1
Yunke Li <yunkeli.work@gmail.com>
2023-09-26 22:14:21 -0700

    develop confirm screen

--- screens/ConfirmScreen.js
+++ screens/ConfirmScreen.js
@@ -7,9 +7,12 @@ const ConfirmScreen = ({ userData: {name, email, phone}, onGoBack, onConfirm }) 
   return (
     <View>
       <CardComp>
-        <Text>Confirm Page</Text>
-        <Button title="Go Back" onPress={onGoBack} />
-        <Button title="Confirm" onPress={onConfirm} />
+        <Text>Hello {name}</Text>
+        <Text>Please confirm the following information is correct by pressing the continue button</Text>
+        <Text>{email}</Text>
+        <Text>{phone}</Text>
+        <Button title="Go back" onPress={onGoBack} />
+        <Button title="Continue" onPress={onConfirm} />
       </CardComp>
     </View>
   );
